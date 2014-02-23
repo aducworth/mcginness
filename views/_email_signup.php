2351e5bcfcc8b5f7c0f6d2020acd097c94bed7be
Austin Ducworth <austin@ehousestudio.com>
2014-02-23 17:42:45 -0500

    updated to email newsletter signup
    Signed-off-by: Austin Ducworth <austin@ehousestudio.com>

--- views/_email_signup.php
+++ views/_email_signup.php
@@ -2,8 +2,46 @@
 
 	<h1>Newsletter Signup</h1>
 	
+	<span class='result errors' style='font-family: Helvetica'></span>
+	
 	<p><input type='text' name='email' placeholder='Email'></p>
 	
 	<button id='submit' class='red-button'>Signup</button>
 	
 </form>
+
+<script>
+
+	$(document).ready(function(){
+
+		$('#newsletter-signup').submit(function(e){
+		
+			e.preventDefault();
+			
+			var postData = $(this).serialize();
+			
+			$.ajax({
+			  url: '/_send_email?ajax=true',
+			  data: postData,
+			  type: "POST"
+			}).done(function(data) {
+			
+			  if( data == 'success' ) {
+				  
+				  $('.result').html('Thanks for signing up!');
+				  
+			  } else {
+				  
+				  $('.result').html('Please provide a valid email.');
+			  }
+			  
+			  $.colorbox.resize();
+			  
+			});
+			
+		
+		});
+			
+	});
+
+</script>
---
+++ views/_send_email.php
@@ -0,0 +1,22 @@
+<? 
+
+	$db = new DB;
+	$store = new Store( $db );
+
+	if( $_POST['email'] ) {
+		
+		// send confirmation emails
+		$to = 'aducworth@gmail.com';
+		$subject = 'Newsletter Signup';
+		$body = 'This email has signed up for your newsletter: ' . $_POST['email'];
+		$store->send_mail( $to, $body, $subject, 'store@boxworkcabinets.com', 'Boxwork Store' );
+		
+		echo( 'success' );
+
+	} else {
+		
+		echo( 'error' );
+		
+	}
+		
+?>
